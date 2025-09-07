8c45c21c0f647c91d2873b68e43c96873775716a
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-09-07 09:24:19 +0300

    feat: post is tested with Postman works fine

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import type { CreateUserDto } from './users.service';
 
 @Controller('users') // Bu controller'ın /users yolunu dinleyeceğini belirtir
 export class UsersController {
@@ -9,5 +17,14 @@ export class UsersController {
   @Get() // GET isteği /users yoluna geldiğinde bu metod çalışır
   findAll() {
     return this.usersService.findAll();
+  }
+  @Get(':id') // /users/1, /users/2 gibi istekleri yakalar
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    // ParseIntPipe: URL'den gelen 'id' parametresinin string'den number'a güvenli bir şekilde dönüştürülmesini sağlar.
+    return this.usersService.findOne(id);
+  }
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.create(userDto);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 // Frontend'deki type'a benzer bir interface oluşturalım
 export interface User {
   id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+// Yeni kullanıcı oluştururken frontend'den gelecek verinin tipini tanımlayalım (id'siz)
+export interface CreateUserDto {
   name: string;
   username: string;
   email: string;
@@ -31,9 +38,28 @@ export class UsersService {
       email: 'Nathan@yesenia.net',
     },
   ];
+  private currentId = this.users.length;
 
   // Tüm kullanıcıları döndüren metod
   findAll(): User[] {
     return this.users;
+  }
+
+  // YENİ METOD
+  findOne(id: number): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+  // DTO gelen verinin şeklini(shape) (Data transfer Object deseni).
+  create(userDto: CreateUserDto): User {
+    const newUser = {
+      id: ++this.currentId,
+      ...userDto,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 }
